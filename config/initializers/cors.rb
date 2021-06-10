6515de3b6d1ca5f21d28e4b8eca0b38616caac87
江原巧 <e.t.miiii228@gmail.com>
2021-06-10 17:17:32 +0900

    cors の設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins Settings.frontend.url
     resource "*",
              headers: :any,
+             expose: ["access-token", "expiry", "token-type", "uid", "client"],
              methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
